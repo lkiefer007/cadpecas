3b15019b6d41e1a242e0bd8bba72d84f6e199e60
Leandro Kiefer <106201435+lkiefer007@users.noreply.github.com>
2022-06-17 22:09:14 -0300

    Add files via upload

---
+++ cadPecas.js
@@ -0,0 +1,19 @@
+let listasQtdPecas = 10;
+
+let peso = 50;
+let nomePeca = "pc";
+
+if(peso>=100) {
+    console.log("peso da peça possui requisitos para cadastrar");
+}else{
+    console.log("peso insuficiente")
+}
+
+if(listasQtdPecas >= 10){
+    console.log("não há capacidade na lista: Caixa Lotada!!!");
+}
+
+if(nomePeca.length <3) {
+    console.log("Nome Inválido para o cadatro da peça: Precisa conter pelo menos 3 caracteres");
+}
+https://github.com/lkiefer007/exercicioremoto.git
